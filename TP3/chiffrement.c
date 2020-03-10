5ac763da661293baa3bdcfa7da4debba84e226c0
LetMax <leterremaxime@gmail.com>
2020-03-10 09:24:58 +0100

    chiffrement

--- TP3/chiffrement.c
+++ TP3/chiffrement.c
@@ -3,7 +3,7 @@
 #include <ctype.h>
 #include <string.h>
 
-char* vigenere(char* claire, char cle);
+char* vigenere(char* claire, char* cle);
 char cesar(char clair, char clee);
 char* substr(char* src, int pos, int len);
 char* simplifier_chaine(char* chaine);
@@ -11,32 +11,35 @@ char* simplifier_chaine(char* chaine);
 int main(int argc, char* agrv[]){
 
     //chaine à chiffrée
-    char* chaine = malloc(500);
+    char* chaine = malloc(500*sizeof(char));
     printf("Entrez un texte que vous voulez chiffrer : ");
-    scanf("%s", chaine);
+    scanf("%[^\n]s", chaine);
 
     while(getchar()!='\n');
 
-    char cle;
-    printf("Entrez la clée de chiffrement (un caractère): ");
-    scanf("%c", &cle);
+    char* cle = malloc(500*sizeof(char));
+    printf("Entrez la clée de chiffrement : ");
+    scanf("%s", cle);
 
     char* claire = simplifier_chaine(chaine);
 
+    printf("Texte non chiffre : %s\n", claire);
+
     char* chiffre = vigenere(claire, cle);
 
-    printf("Texte non chiffre : %s\n", claire);
     printf("Texte chiffre : %s\n", chiffre);
 }
 
-char* vigenere(char* claire, char cle){
+char* vigenere(char* claire, char* cle){
     char* chiffre = malloc(sizeof(claire));
     strcpy(chiffre, claire);
     int i = 0;
+
     while(chiffre[i]){
-        chiffre[i] = cesar(claire[i], cle);
+        chiffre[i] = cesar(claire[i], cle[i%strlen(cle)]);
         i++;
     }
+
     return chiffre;
 }
 
@@ -50,7 +53,7 @@ char cesar(char clair, char cle){
 }
 
 char* substr(char* src, int pos, int len) {
-    char *dest = NULL;
+    char* dest = NULL;
     if(len > 0) {
         dest = (char *) malloc(len+1);
         strncat(dest, src+pos, len);
@@ -60,7 +63,7 @@ char* substr(char* src, int pos, int len) {
 
 char* simplifier_chaine(char* chaine){
 
-    char* resultat = malloc(sizeof(chaine));
+    char* resultat = malloc(strlen(chaine));
     strcpy(resultat, chaine);
     int i = 0;
     while(resultat[i]){
